6674090ddf52d9e81d9c3a7dac1fb350926f0c98
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-16 22:05:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RemoveDuplicateAssessments
+{
+    using System.Collections.Generic;
+
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "apple", "banana", "apple", "orange", "banana", "grape" };
+
+            List<string> uniqueList = RemoveDuplicates(list);
+
+            foreach (string item in uniqueList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<T> RemoveDuplicates<T>(List<T> list)
+        {
+            HashSet<T> uniqueSet = new HashSet<T>();
+
+            foreach (T item in list)
+            {
+                uniqueSet.Add(item);
+            }
+
+            return new List<T>(uniqueSet);
+        }
+    }
+}
